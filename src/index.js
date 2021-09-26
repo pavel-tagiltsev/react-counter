a78985ca620007cb50f20dfd2189820ab23cf156
Павел <pavel.tagiltsef@gmail.com>
2021-09-26 09:26:06 +0300

    Создает простое приложение счетчие с использованием redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const setEventListener = (selector, action) => {
+    document.getElementById(selector).addEventListener('click', () => {
+        store.dispatch(action());
+    })
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const reducer = (state = 0, action) => {
+    switch (action.type) {
+        case 'INC':
+            return state + 1;
+        case 'DEC':
+            return state - 1;
+        case 'RES':
+            return state - state;
+        default:
+            return state;
+    }
+}
+
+const inc = () => ({type: 'INC'});
+const dec = () => ({type: 'DEC'});
+const res = () => ({type: 'RES'});
+
+const store = createStore(reducer);
+
+document.getElementById('counter').textContent = store.getState();
+
+store.subscribe(() => {
+    document.getElementById('counter').textContent = store.getState();
+})
+
+setEventListener('inc', inc);
+setEventListener('dec', dec);
+setEventListener('res', res);
+
+
+
